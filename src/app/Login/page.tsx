cf7c0d84bdd170bc17fc5e52090be12000af1d78
baibhav <baibhavtiwari37@gmail.com>
2023-07-10 22:37:44 +0530

    routing done

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,24 +1,51 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import {axios} from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 
 
 export default function  signup() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email : "",
-    password : "",
-  
+    password: "",
   })
+
+  const [buttonDisabled, setButtonDisabled] = React.
+    useState(false);
+  const [loading, setLoading] = React.useState(false);
+
   const onLogin =async()=>{
-    
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("login Success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login Failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   }
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+
 
   return (
     <div className ='flex flex-col items-center justify-center min-h-screen py-4'>
-      <h1 className="text-2xl py-4">Login</h1>
+      <h1 className="text-2xl py-4">{loading ? "Processing" : "Login"}</h1>
       <hr/>
       <label htmlFor="username">Email</label>
       <input
--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,7 +2,7 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import bcrypt from "bcryptjs/dist/bcrypt";
+
 
 
 connect()
